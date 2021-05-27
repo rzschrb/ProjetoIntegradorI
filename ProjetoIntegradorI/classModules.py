938a755aedfbcd643a8133d0fef99a4e6d37a5f0
Davi-SO <80834681+Davi-SO@users.noreply.github.com>
2021-05-27 10:51:55 -0300

    Update classModules.py

--- ProjetoIntegradorI/classModules.py
+++ ProjetoIntegradorI/classModules.py
@@ -14,27 +14,29 @@ class SistemaRH:
 
     def calc_inss(self, salarioBruto):
 
-        salINSS = [1100, 2203.48, 3305.22, 6433.57] # Lista de divisão de salários - INSS
-        aliINSS = [7.50, 9, 12, 14] # Lista de divisão de aliquotas em relação aos salários - INSS
+        salINSS = [1100, 2203.48, 3305.22, 6433.57]  # Lista de divisão de salários - INSS
+        aliINSS = [7.50, 9, 12, 14]  # Lista de divisão de aliquotas em relação aos salários - INSS
         parINSS = [82.5, 99.31, 132.20]
 
         taxaINSS = 0
         salarioBrutoB = salarioBruto
 
-        if (self.salarioBruto <= salINSS[0]): # Salário igual ou menor a 1100
-            taxaINSS = salarioBruto * (aliINSS[0]/100)
-        elif (self.salarioBruto <= salINSS[1]): # Salário igual ou menor a 2203,48
+        if (self.salarioBruto <= salINSS[0]):  # Salário igual ou menor a 1100
+            taxaINSS = salarioBruto * (aliINSS[0] / 100)
+        elif (self.salarioBruto <= salINSS[1]):  # Salário igual ou menor a 2203,48
             salarioBrutoB = self.salarioBruto - salINSS[0]
-            taxaINSS = salarioBrutoB * (aliINSS[1]/100) + parINSS[0]
-        elif (self.salarioBruto <= salINSS[2]): # Salário igual ou menor a 3305,22
+            taxaINSS = salarioBrutoB * (aliINSS[1] / 100) + parINSS[0]
+        elif (self.salarioBruto <= salINSS[2]):  # Salário igual ou menor a 3305,22
             salarioBrutoB = self.salarioBruto - salINSS[1]
-            taxaINSS = salarioBrutoB * (aliINSS[2]/100) + (parINSS[0] + parINSS[1])
-        elif (self.salarioBruto <= salINSS[3]): # Salário igual ou menor a 6433,57
+            taxaINSS = salarioBrutoB * (aliINSS[2] / 100) + (parINSS[0] + parINSS[1])
+        elif (self.salarioBruto <= salINSS[3]):  # Salário igual ou menor a 6433,57
             salarioBrutoB = self.salarioBruto - salINSS[2]
-            taxaINSS = salarioBrutoB * (aliINSS[3]/100) + (parINSS[0] + parINSS[1] + parINSS[2])
-        else: # Salário acima do teto do INSS
-            taxaINSS = (salINSS[0]*(aliINSS[0]/100)) + ((salINSS[1]-salINSS[0])*(aliINSS[1]/100)) + ((salINSS[2]-salINSS[1])*(aliINSS[2]/100)) + ((salINSS[3]-salINSS[2])*(aliINSS[3]/100))     
-        
+            taxaINSS = salarioBrutoB * (aliINSS[3] / 100) + (parINSS[0] + parINSS[1] + parINSS[2])
+        else:  # Salário acima do teto do INSS
+            taxaINSS = (salINSS[0] * (aliINSS[0] / 100)) + ((salINSS[1] - salINSS[0]) * (aliINSS[1] / 100)) + (
+                        (salINSS[2] - salINSS[1]) * (aliINSS[2] / 100)) + (
+                                   (salINSS[3] - salINSS[2]) * (aliINSS[3] / 100))
+
         self.taxaINSS = taxaINSS
 
         return self.taxaINSS
@@ -44,27 +46,27 @@ class SistemaRH:
 
     def calc_irrf(self, salarioBruto, taxaINSS):
 
-        salIRRF = [1903.98, 2826.65, 3751.05, 4664.68] # Lista de divisão de salários - IRRF
-        aliIRRF = [0, 7.5, 15, 22.5, 27.5] # Lista de divisão de aliquotas em relação aos salários - IRRF
-        parIRRF = [0, 142.80, 354.8, 636.13, 869.36] # Lista de divisão de dedução do IRRF referente aos salários
+        salIRRF = [1903.98, 2826.65, 3751.05, 4664.68]  # Lista de divisão de salários - IRRF
+        aliIRRF = [0, 7.5, 15, 22.5, 27.5]  # Lista de divisão de aliquotas em relação aos salários - IRRF
+        parIRRF = [0, 142.80, 354.8, 636.13, 869.36]  # Lista de divisão de dedução do IRRF referente aos salários
 
         taxaIRRF = 0
 
-        if ((salarioBruto - taxaINSS) <= salIRRF[0]): # Salário igual ou menor a 1903,98.
+        if ((salarioBruto - taxaINSS) <= salIRRF[0]):  # Salário igual ou menor a 1903,98.
             taxaIRRF = (salarioBruto * 0)
-        elif ((salarioBruto - taxaINSS) <= salIRRF[1]): # Salário igual ou menor a 2826,65.
-            taxaIRRF = (salarioBruto * (aliIRRF[1]/100)) - parIRRF[1]
-        elif ((salarioBruto - taxaINSS) <= salIRRF[2]): # Salário igual ou menor a 3751,05.
-            taxaIRRF = (salarioBruto * (aliIRRF[2]/100)) - parIRRF[2]
-        elif ((salarioBruto - taxaINSS) <= salIRRF[3]): # Salário igual ou menor a 4664,68.
-            taxaIRRF = (salarioBruto * (aliIRRF[3]/100)) - parIRRF[3]
-        else: # Salário maior que 4664,68.
-            taxaIRRF = (salarioBruto * (aliIRRF[4]/100)) - parIRRF[4]
+        elif ((salarioBruto - taxaINSS) <= salIRRF[1]):  # Salário igual ou menor a 2826,65.
+            taxaIRRF = (salarioBruto * (aliIRRF[1] / 100)) - parIRRF[1]
+        elif ((salarioBruto - taxaINSS) <= salIRRF[2]):  # Salário igual ou menor a 3751,05.
+            taxaIRRF = (salarioBruto * (aliIRRF[2] / 100)) - parIRRF[2]
+        elif ((salarioBruto - taxaINSS) <= salIRRF[3]):  # Salário igual ou menor a 4664,68.
+            taxaIRRF = (salarioBruto * (aliIRRF[3] / 100)) - parIRRF[3]
+        else:  # Salário maior que 4664,68.
+            taxaIRRF = (salarioBruto * (aliIRRF[4] / 100)) - parIRRF[4]
 
         self.taxaIRRF = taxaIRRF
 
         return self.taxaIRRF
-    
+
     def get_taxaIRRF(self):
         return self.taxaIRRF
 
@@ -99,7 +101,7 @@ class SistemaRH:
                 print(f"Pagamento referente a 1/3 das Férias Liquidas (Abono Pecuniário): R$ {segundoValor:.2f}")
                 print(f"Total bruto com o 1/3: R$ {valorBruto:.2f}")
                 print(f"Total líquido: R$ {valorTotal:.2f}")
-            
+
         except ValueError:
             return "O valor deve ser númerico."
 
@@ -107,23 +109,20 @@ class SistemaRH:
 
         while True:
             try:
-                nmt = float(input('Tempo total de trabalho esse ano (em meses): '))
+                nmt = float(input('Tempo total de trabalho desde outubro do ano passado (em meses): '))
                 if nmt <= 0:
                     raise
-                else: 
+                else:
                     break
-                nmto = nmt - 2
 
-                valorPrimario = (self.salarioBruto / 12) * nmto
+                valorPrimario = (self.salarioBruto / 12) * nmt
                 primeiraParcela = valorPrimario / 2
-
-                valorSecundario = (self.salarioBruto / 12) * nmt
-                segundaParcela = valorSecundario - (primeiraParcela + self.taxaINSS + self.taxaIRRF)
+                segundaParcela = primeiraParcela - (self.taxaINSS + self.taxaIRRF)
 
                 print(f'O funcionário vai receber: ')
                 print(f'Primeira Parcela: R$ {primeiraParcela:.2f}')
                 print(f'Segunda Parcela: R$ {segundaParcela:.2f}')
-                print(f'Total: R${primeiraParcela+segundaParcela}')
+                print(f'Total: R${primeiraParcela + segundaParcela}')
 
             except ValueError:
                 print("Erro no valor")
